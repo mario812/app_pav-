8aeb5e46b953de04383721e4b64b43eb2b642196
mario812 <mlct_018@hotmail.com>
2024-04-28 08:20:43 -0400

    feat: actualicé  await Firebase.initializeApp(     options: DefaultFirebaseOptions.     currentPlatform) con el argumento que contiene     actualmente. Este método no funcionaba sin     este argumento

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
 import 'package:firebase_core/firebase_core.dart';
+//import 'package:flutter/foundation.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // if (kIsWeb) {
+  //   // Inicialización para web
+  //   await Firebase.initializeApp(
+  //     [
+  //       FirebaseAppOptions(
+  //         apiKey: "AIzaSyC3jT1f7qEOFcfMWzeBwiK8XTkKNcSA-pw",
+  //         appId: "1:798407068989:web:f7dea619ca9506987a8954",
+  //         messagingSenderId: "798407068989",
+  //         projectId: "proyecto-pad",
+  //         storageBucket: "proyecto-pad.appspot.com",
+  //       )
+  //     ],
+  //   );
+  // } else {
+  //   // Inicialización para dispositivos móviles (opcional)
+  // }
   runApp(const Registro());
 }
 
